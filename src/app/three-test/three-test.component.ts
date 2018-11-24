fd8b0a755a1f8fbd67c1b3c6a7e06e05a1a339ae
Johan 't Hart <johanthart@gmail.com>
2018-11-25 00:15:00 +0100

    Add ThreeTest component

---
+++ src/app/three-test/three-test.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-three-test',
+  templateUrl: './three-test.component.html',
+  styleUrls: ['./three-test.component.css']
+})
+export class ThreeTestComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}

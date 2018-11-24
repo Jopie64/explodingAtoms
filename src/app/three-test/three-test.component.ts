8e46195a9c6e5ddde1abd2419e73fc584e0ecfda
Johan 't Hart <johanthart@gmail.com>
2018-11-25 00:28:19 +0100

    Setup rotating cube

--- src/app/three-test/three-test.component.ts
+++ src/app/three-test/three-test.component.ts
@@ -1,15 +1,47 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild, ElementRef, AfterViewInit } from '@angular/core';
+import * as THREE from 'three';
+
+// Following this example: https://stackoverflow.com/questions/40273300/angular-cli-threejs
 
 @Component({
   selector: 'app-three-test',
   templateUrl: './three-test.component.html',
   styleUrls: ['./three-test.component.css']
 })
-export class ThreeTestComponent implements OnInit {
+export class ThreeTestComponent implements OnInit, AfterViewInit {
 
-  constructor() { }
+  @ViewChild('rendererContainer') rendererContainer: ElementRef;
+
+  renderer = new THREE.WebGLRenderer();
+  scene = null;
+  camera = null;
+  mesh = null;
+
+  constructor() {
+    this.scene = new THREE.Scene();
+    this.camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 1, 10000);
+    this.camera.position.z = 1000;
+
+    const geometry = new THREE.BoxGeometry(200, 200, 200);
+    const material = new THREE.MeshBasicMaterial({color: 0xff0000, wireframe: true});
+    this.mesh = new THREE.Mesh(geometry, material);
+
+    this.scene.add(this.mesh);
+  }
 
   ngOnInit() {
   }
 
+  ngAfterViewInit() {
+    this.renderer.setSize(window.innerWidth, window.innerHeight);
+    this.rendererContainer.nativeElement.appendChild(this.renderer.domElement);
+    this.animate();
+  }
+
+  animate() {
+    window.requestAnimationFrame(() => this.animate());
+    this.mesh.rotation.x += 0.01;
+    this.mesh.rotation.y += 0.02;
+    this.renderer.render(this.scene, this.camera);
+  }
 }
